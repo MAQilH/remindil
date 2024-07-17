8bb54a520bb7d7e5eb849e1f44a9b9234a9f9292
AQil <mohammada.hafezi@gmail.com>
2024-07-18 01:38:01 +0330

    add nav bar

---
+++ src/app/add-file-button/add-file-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-file-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-file-button.component.html',
+  styleUrl: './add-file-button.component.scss'
+})
+export class AddFileButtonComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { ExplorerComponent } from "./explorer/explorer.component";
+import { AddFileButtonComponent } from "./add-file-button/add-file-button.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToolbarComponent, ExplorerComponent, AddFileButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
